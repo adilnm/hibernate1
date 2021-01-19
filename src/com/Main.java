fc2061bd899fafca1a38209f280d459db360c141
Adil Naimi <adilnm23@gmail.com>
2021-01-19 00:46:08 -0600

    Add qhuerries

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -28,8 +29,51 @@ public class Main {
 //		long id = (long) session.save(a1);
 //		System.out.println(session.save(a1));
 
-		Author a1 = (Author) session.load(Author.class, 2L);
-		System.out.println(a1);
+//		Author a1 = (Author) session.load(Author.class, 2L);
+//		System.out.println(a1);
+
+//		Book b1 = new Book("Java", "good", new Date());
+//		long id = (long) session.save(b1);
+
+		// delete one author
+//		Author a3 = (Author) session.get(Author.class, 7L);
+//		session.delete(a3);
+
+		// update
+//		Author a3 = (Author) session.get(Author.class, 5L);
+//		a3.setName("John");
+//		a3.setEmail("john@gmail.com");
+//		session.update(a3);
+
+		// fetch all authors
+//		String hql = " from Author";// here Author is class name
+//		Query query = session.createQuery(hql);
+//		List<Author> listOfAuthors = query.list();
+//		System.out.println(listOfAuthors);
+//
+//		for (Author author : listOfAuthors) {
+//			System.out.println(author.getName());
+//		}
+
+		// search(select on basis of some conditions)
+
+//		String hql = "from Author where name =:name"; // named parameter
+//		Query query = session.createQuery(hql);
+//		query.setParameter("name", "Adil"); // recommended
+//		List<Author> listOfAuthors = query.list();
+//
+//		for (Author author : listOfAuthors) {
+//			System.out.println(author.getName());
+//		}
+
+		// update
+
+		String hql = "update Author set email = :email where id = :id";
+		Query query = session.createQuery(hql);
+		query.setParameter("email", "j@gmail.com");
+		query.setParameter("id", 5L);
+
+		query.executeUpdate();
 
 		session.getTransaction().commit();
 		session.close();
